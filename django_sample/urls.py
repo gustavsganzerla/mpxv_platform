7585ec24852cc01cdd8da2a8e647b16ce5df6dd6
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2024-12-02 08:51:10 -0400

    urls

--- django_sample/urls.py
+++ django_sample/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('my_app/', include('my_app.urls'))
 ]
---
+++ my_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("test_view/", views.test_view, name = "test_view")
+    
+]
